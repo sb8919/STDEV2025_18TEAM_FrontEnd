861266d742acfb1d6e503f4280d31d98ceec4ce2
SangbumPark <sb8919@naver.com>
2025-04-19 02:55:52 +0900

    (feat) 카드뉴스 ui 제작

--- lib/screens/card_news_screen.dart
+++ lib/screens/card_news_screen.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import '../constants/card_data.dart';
+import '../constants/app_colors.dart';
 
 class CardNewsScreen extends StatelessWidget {
   const CardNewsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("카드뉴스 화면"));
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: cardData.length,
+      itemBuilder: (context, index) {
+        final news = cardData[index];
+        return Container(
+          margin: const EdgeInsets.only(bottom: 16),
+          height: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            image: DecorationImage(
+              image: AssetImage(news['image']!),
+              fit: BoxFit.cover,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 8,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.transparent,
+                  Colors.black.withOpacity(0.7),
+                ],
+              ),
+            ),
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  news['title']!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 } 
